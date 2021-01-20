dee1ed48d37e46ea0dd0937f7dad93ae6c453982
Pro <pro@Pros-MacBook-Pro.local>
2021-01-20 08:00:18 +0800

    Homescreen product listing

--- front-end/src/screens/HomeScreen.js
+++ front-end/src/screens/HomeScreen.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 import products from '../products';
-export const HomeScreen = () => {
+import Product from '../components/Product';
+const HomeScreen = () => {
 	return (
 		<div>
 			<h1>The Latest Products</h1>
+			<Row>
+				{products.map((product) => (
+					<Col sm={12} md={6} lg={4} xl={3}>
+						<Product product={product} />
+					</Col>
+				))}
+			</Row>
 		</div>
 	);
 };
+export default HomeScreen;
