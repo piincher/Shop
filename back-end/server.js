5b8d9a019ece47ae69ef05a6a81cffd4e524b40f
Pro <pro@Pros-MacBook-Pro.local>
2021-01-21 13:35:14 +0800

    implementing sample data users

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
 import connectDB from './config/db.js';
 import products from './data/products.js';
 
@@ -21,4 +22,4 @@ app.get('/api/products/:id', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`server runnig in ${process.env.NODE_ENV} mode on port  ${PORT}`));
+app.listen(PORT, console.log(`server runnig in ${process.env.NODE_ENV} mode on port  ${PORT}`.yellow.bold));
