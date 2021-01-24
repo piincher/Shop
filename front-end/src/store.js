0b19f5b9be7de14b77b799e52ada6540f5175b9d
Pro <pro@Pros-MacBook-Pro.local>
2021-01-24 17:03:57 +0800

    cart reducer and to cart in our application

--- front-end/src/store.js
+++ front-end/src/store.js
@@ -2,11 +2,16 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { cartReducer } from './reducers/cartReducers';
 const reducer = combineReducers({
 	productList: productListReducer,
-	productDetails: productDetailsReducer
+	productDetails: productDetailsReducer,
+	cart: cartReducer
 });
-const initialState = {};
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
+const initialState = {
+	cart: { cartItems: cartItemsFromStorage }
+};
 const middleware = [ thunk ];
 const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
 export default store;
