56f57fc8ee2276a7f0422658c9a458b146b8c5bb
Pro <pro@Pros-MacBook-Pro.local>
2021-01-24 15:09:37 +0800

    quantity select and add cart button in the application

---
+++ front-end/src/components/Loader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Spinner } from 'react-bootstrap';
+const Loader = () => {
+	return (
+		<Spinner
+			animation="grow"
+			role="status"
+			style={{ width: '100px', height: '100px', margin: 'auto', display: 'block' }}
+		>
+			<span className="sr-only">Chargement...</span>
+		</Spinner>
+	);
+};
+
+export default Loader;
---
+++ front-end/src/screens/CartScreen.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const CartScreen = () => {
+	return <div>Cart screen</div>;
+};
+export default CartScreen;
