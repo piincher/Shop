1bfe97112f7fe18b496c353f9cbfbada7f2b97b4
Pro <pro@Pros-MacBook-Pro.local>
2021-01-29 16:39:40 +0800

    implement user login screen and user functionnalities

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -20,7 +20,7 @@ const authUser = asyncHandler(async (req, res) => {
 		});
 	} else {
 		res.status(401);
-		throw new Error('Invalid email or password');
+		throw new Error('Email ou Mot de passe invalide');
 	}
 });
 
