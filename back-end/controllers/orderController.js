e467d1ad50dfe61fada8e0721933fefb38edd104
Pro <pro@Pros-MacBook-Pro.local>
2021-02-02 17:48:43 +0800

    order controller and routes

---
+++ back-end/controllers/orderController.js
@@ -0,0 +1,30 @@
+import asyncHandler from 'express-async-handler';
+import Order from '../models/orderModel.js';
+
+// @desc    Create new Order
+// @route   POST  /api/orders
+// @access  Private
+const addOrderItems = asyncHandler(async (req, res) => {
+	const { orderItems, shippingAddress, paymentMethod, itemsPrice, taxPrice, shippingPrice, totalPrice } = req.body;
+
+	if (orderItems && orderItems.length === 0) {
+		res.status(400);
+		throw new Error('no order Items ');
+		return;
+	} else {
+		const order = new Order({
+			orderItems,
+			shippingAddress,
+			user: req.user._id,
+			paymentMethod,
+			itemsPrice,
+			taxPrice,
+			shippingPrice,
+			totalPrice
+		});
+
+		const createdOrder = await order.save();
+		res.status(201).json(createdOrder);
+	}
+});
+export { addOrderItems };
