46c25cdc0d5cd91d25b922179a201bc7a054f485
Pro <pro@Pros-MacBook-Pro.local>
2021-02-05 14:23:47 +0800

    admin middleware and get users and point

--- front-end/src/actions/userActions.js
+++ front-end/src/actions/userActions.js
@@ -11,8 +11,10 @@ import {
 	USER_REGISTER_SUCCESS,
 	USER_UPDATE_PROFILE_FAIL,
 	USER_UPDATE_PROFILE_REQUEST,
-	USER_UPDATE_PROFILE_SUCCESS
+	USER_UPDATE_PROFILE_SUCCESS,
+	USER_DETAILS_RESET
 } from '../constants/userConstants';
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants';
 import axios from 'axios';
 export const login = (email, password) => async (dispatch) => {
 	try {
@@ -46,6 +48,8 @@ export const logout = () => async (dispatch) => {
 	dispatch({
 		type: USER_LOGOUT
 	});
+	dispatch({ type: USER_DETAILS_RESET });
+	dispatch({ type: ORDER_LIST_MY_RESET });
 };
 
 export const register = (name, email, password) => async (dispatch) => {
