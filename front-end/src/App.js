3c33faf94cdcb9a168652f22674903acd92051af
Pro <pro@Pros-MacBook-Pro.local>
2021-02-08 15:13:31 +0800

    Edit product screen

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -16,6 +16,7 @@ import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 const App = () => {
 	return (
 		<Router>
@@ -33,8 +34,10 @@ const App = () => {
 					<Route path="/profile" component={ProfileScreen} />
 					<Route path="/shipping" component={ShippingScreen} />
 					<Route path="/payment" component={PaymentScreen} />
-					<Route path="/" exact component={HomeScreen} />
+
 					<Route path="/admin/productlist" component={ProductListScreen} />
+					<Route path="/admin/product/:id/edit" component={ProductEditScreen} />
+					<Route path="/" exact component={HomeScreen} />
 				</Container>
 			</main>
 
