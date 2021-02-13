3d72730c61330f56217b054ac88925b946a5eda5
Pro <ikouma269@gmail.com>
2021-02-13 12:21:06 +0800

    restyling the entire web

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
 
 	return (
 		<header>
-			<Navbar bg="light" variant="dark" expand="lg" collapseOnSelect>
+			<Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
 				<Container>
 					<LinkContainer to="/">
 						<Navbar.Brand>MYEMPIREBYMYMA</Navbar.Brand>
